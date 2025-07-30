4b74b5c30984559f40f5043b62f0304397939422
Blein Alem <bleinalem@gmail.com>
2025-07-29 21:32:19 -0400

    Fixed keypair errors

--- main.tf
+++ main.tf
@@ -206,7 +206,6 @@ resource "aws_lb" "web_alb" {
   name               = "web-alb"
   internal           = false
   load_balancer_type = "application"
-  subnets            = [aws_subnet.public.id]
   security_groups    = [aws_security_group.alb_sg.id]
 
   tags = {
