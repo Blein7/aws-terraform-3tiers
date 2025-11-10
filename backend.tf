90fa58969e54e1ff2237a7ae30cc290cba5bc29e
Blein Alem <bleinalem@gmail.com>
2025-11-09 22:21:55 -0500

    Deploying project

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-backend-tf-bucket17"
+    bucket         = "my-backend-tf-bucket178"
     key            = "3-tier-project/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock-table17"
