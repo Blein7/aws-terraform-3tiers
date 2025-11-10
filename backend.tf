d82df0bd5702200acd33bb1a9ba4c5f1e7cd1cec
Blein Alem <bleinalem@gmail.com>
2025-11-09 23:02:52 -0500

    Updates.yml files

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-backend-tf-bucket178"
+    bucket         = "my-backend-tf-bucket1787"
     key            = "3-tier-project/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock-table17"
